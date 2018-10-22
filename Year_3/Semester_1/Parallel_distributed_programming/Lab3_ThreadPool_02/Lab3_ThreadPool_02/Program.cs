266e4fbc81a0ab500f764decef3a4793d7a48aeb
Stefan Delibas <delibas.stefan@gmail.com>
2018-10-23 01:17:36 +0300

    lab3 ppd 50%

---
+++ Year_3/Semester_1/Parallel_distributed_programming/Lab3_ThreadPool_02/Lab3_ThreadPool_02/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab3_ThreadPool_02
+{
+
+    class Program
+    {
+        static int rows = 160;
+        static int columns = 160;
+        static int[] temp = new int[rows * columns];
+        static int[,] matrixA = new int[rows, columns];
+
+        static int[,] matrixB = new int[rows, columns];
+        static int[,] result = new int[rows, columns];
+
+        static void RunMultiplication(object obj)
+        {
+
+            int index = (int)obj;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    result[index, i] += matrixA[index, j] * matrixB[j, i];
+                }
+            }
+            
+        }
+
+        static void FillMatrix()
+        {
+            for (int i = 0; i < matrixA.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrixA.GetLength(1); j++)
+                {
+                    matrixA[i, j] = 1;
+                    matrixB[i, j] = 2;
+                }
+            }
+
+        }
+
+        static void MultiplyMatrices()
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(RunMultiplication), i);
+            }
+
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(string.Format("{0}    ", matrix[i, j]));
+                }
+                Console.Write(Environment.NewLine + Environment.NewLine);
+            }
+        }
+
+        static void Main(String[] args)
+        {
+            FillMatrix();
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            MultiplyMatrices();
+            sw.Stop();
+
+            //PrintMatrix(result);
+            Console.WriteLine("Elapsed={0}", sw.ElapsedMilliseconds);
+            Console.ReadKey();
+        }
+    }
+}
+
